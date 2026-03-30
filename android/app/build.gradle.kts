b75808ec39bf9fa00fe04d0cfa845916b39897a3
Tanya Garg <tgarg1012@gmail.com>
2026-03-30 23:55:38 +0530

    Added custom app icon for DocuVault AI using flutter_launcher_icons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -19,7 +20,10 @@ android {
     }
 
     defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.evidence_locker"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -28,8 +32,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false   // 🔥 FIX
-            isShrinkResources = false // 🔥 FIX
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
